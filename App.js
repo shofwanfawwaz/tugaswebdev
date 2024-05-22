640d7f9cf25ca6cd59664d456424e0a779ed870a
shofwanfawwaz <162024272+shofwanfawwaz@users.noreply.github.com>
2024-05-22 23:49:49 +0700

    Activity 2.1

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import GoalList from './GoalList';
-import Header from './Header';
+import React from 'react';
+import './App.css';
+
 function App() {
-return (
-<>
-<Header />
-<GoalList />
-</>
-);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, this is me - Fawwaz^__^!</h1>
+        <p>Right now, I am 19 years old and I live in Jakarta.</p>
+        <p>My full name is  Shofwan Fawwaz and I am a College Student at Padjadjaran University Studying Digital Business.I like making music</p>
+      </header>
+    </div>
+  );
 }
-export default App;
+
+export default App;
